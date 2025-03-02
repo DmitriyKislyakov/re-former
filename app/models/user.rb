3a4d68128488346ed1fcf9c4eccc1084b01080f9
Dima <ansfer91@gmail.com>
2025-03-03 00:23:23 +0300

    add form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { in: 6..20 }
 end
